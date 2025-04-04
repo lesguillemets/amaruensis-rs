68e9261a460fe6cde955ef07e8faf1bc27c46367
lesguillemets <kgkacko+github@gmail.com>
2025-04-04 19:38:45 +0900

    devdev

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod consts;
 
+const DEBUG: bool = true;
 use opencv::core::CV_VERSION;
 use opencv::highgui::{imshow, wait_key};
 use opencv::imgcodecs::{imread, ImreadModes};
@@ -11,8 +12,9 @@ use consts::*;
 pub fn do_main() {
     eprint_opencv_version();
     let pair = load_images();
-    let scanned = pair.scanned;
-    let paper = pair.source;
+    let ohtsu = pair.apply(to_bw_ohtsu);
+    let scanned = ohtsu.scanned;
+    let paper = ohtsu.source;
     let diff = (&scanned - &paper).into_result().unwrap();
     println!("{paper:?}");
     println!("{scanned:?}");
@@ -31,14 +33,14 @@ fn eprint_opencv_version() {
 fn load_images() -> PaperPair {
     let paper = imread(EXAMPLE_PAPER_PATH, ImreadModes::IMREAD_GRAYSCALE.into()).unwrap();
     let scanned = imread(EXAMPLE_SCANNED_PATH, ImreadModes::IMREAD_GRAYSCALE.into()).unwrap();
-    println!("{paper:?}");
-    println!("{scanned:?}");
-    let mut loaded = PaperPair {
+    if DEBUG {
+        eprintln!("{paper:?}");
+        eprintln!("{scanned:?}");
+    }
+    PaperPair {
         source: paper,
         scanned,
-    };
-    loaded.apply_both(to_bw);
-    loaded
+    }
 }
 
 /// Pair of the original document and scanned (filled) document
@@ -49,9 +51,25 @@ pub struct PaperPair {
 }
 
 impl PaperPair {
-    pub fn apply_both(&mut self, f: fn(&Mat) -> Mat) {
+    pub fn apply(&self, f: fn(&Mat) -> Mat) -> Self {
+        let new_source = f(&self.source);
+        let new_scanned = f(&self.scanned);
+        PaperPair {
+            source: new_source,
+            scanned: new_scanned,
+        }
+    }
+    pub fn apply_inplace(&mut self, f: fn(&Mat) -> Mat) {
         self.source = f(&self.source);
         self.scanned = f(&self.scanned);
+    }
+    pub fn apply_into(self, f: fn(Mat) -> Mat) -> Self {
+        let new_source = f(self.source);
+        let new_scanned = f(self.scanned);
+        PaperPair {
+            source: new_source,
+            scanned: new_scanned,
+        }
     }
 }
 
@@ -65,5 +83,21 @@ fn to_bw(m: &Mat) -> Mat {
         ThresholdTypes::THRESH_BINARY.into(),
     )
     .unwrap();
+    dest
+}
+
+fn to_bw_ohtsu(m: &Mat) -> Mat {
+    let mut dest = Mat::default();
+    let found_thresh = threshold(
+        m,
+        &mut dest,
+        consts::BLACK_WHITE_THRESH,
+        consts::WHITE,
+        ThresholdTypes::THRESH_OTSU.into(),
+    )
+    .unwrap();
+    if DEBUG {
+        eprintln!("Fount threshold: {found_thresh}");
+    }
     dest
 }
