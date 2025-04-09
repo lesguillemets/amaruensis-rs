2444549031b94d7f33371a2624f3b40af568be18
lesguillemets <kgkacko+github@gmail.com>
2025-04-09 15:19:54 +0900

    Move implementation inside trait to function itself
    (So that it can be called more freely)

--- src/orb_flann.rs
+++ src/orb_flann.rs
@@ -12,6 +12,7 @@ use opencv::prelude::*;
 use crate::base::{gather_good_matches_lowe, gather_good_matches_take_n};
 use crate::consts::{LOWE_DEFAULT_THRESHOLD, ORB_ENLARGE_RECT_BY, ORB_FLANN_SHOW_N_BEST_MATCHES};
 use crate::paper_pair::PaperPair;
+use crate::sheet::SheetData;
 
 const DEBUG: bool = true;
 const PREFER_LOWE: bool = true;
@@ -21,102 +22,110 @@ pub trait ORBFlann {
     fn calc_diff(&self);
 }
 
+fn detect_transform_orb_flann(
+    source: &Mat,
+    scanned: &Mat,
+    sheet_data: &SheetData,
+    enlarge_by: i32,
+) -> Mat {
+    // find keypoints
+    let (source_keypoints, source_descriptors) =
+        detect_and_compute_orb(source, Some(sheet_data.gen_detect_mask(source).unwrap()));
+    // for scanned images, the corresponding points should be near that area.
+    // We will search an area expanded a little (by ORB_ENLARGE_RECT_BY).
+    let (scan_keypoints, scan_descriptors) = detect_and_compute_orb(
+        scanned,
+        Some(
+            sheet_data
+                .gen_enlarged_detect_mask(scanned, enlarge_by, enlarge_by)
+                .unwrap(),
+        ),
+    );
+
+    // use flann matcher to find matches
+    let autoindexparams = flann::AutotunedIndexParams::new_def().expect("autotunedindexparams");
+    let flann_matcher = FlannBasedMatcher::new(
+        &Ptr::new(autoindexparams.into()),
+        &Ptr::new(flann::SearchParams::new_def().unwrap()),
+    )
+    .expect("Creating FlannBasedMatcher");
+    let mut src_desc = Mat::default();
+    let mut scan_desc = Mat::default();
+    // https://stackoverflow.com/a/29695032
+    source_descriptors
+        .convert_to_def(&mut src_desc, CV_32F)
+        .unwrap();
+    scan_descriptors
+        .convert_to_def(&mut scan_desc, CV_32F)
+        .unwrap();
+    // use the best matches in the visualisation
+    let bm: Vector<DMatch> = if PREFER_LOWE {
+        let mut matches = Vector::new();
+        flann_matcher
+            .knn_train_match_def(&scan_desc, &src_desc, &mut matches, 2)
+            .unwrap();
+        gather_good_matches_lowe(&matches, LOWE_DEFAULT_THRESHOLD)
+    } else {
+        let mut matches = Vector::new();
+        flann_matcher
+            .train_match_def(&scan_desc, &src_desc, &mut matches)
+            .unwrap();
+        gather_good_matches_take_n(&matches, ORB_FLANN_SHOW_N_BEST_MATCHES)
+    };
+    if DEBUG {
+        let mut result = Mat::default();
+        draw_matches_def(
+            &source,
+            &source_keypoints,
+            &scanned,
+            &scan_keypoints,
+            &bm,
+            &mut result,
+        )
+        .unwrap();
+        imshow("Matching result", &result).unwrap();
+        wait_key(0).unwrap();
+        imwrite(
+            "result.png",
+            &result,
+            &Vector::from_slice(&[ImwriteFlags::IMWRITE_PNG_COMPRESSION.into(), 9]),
+        )
+        .unwrap();
+    }
+
+    // find the transformation matrix
+    let mut from_points: Vector<KeyPoint> = Vector::with_capacity(bm.len());
+    let mut to_points: Vector<KeyPoint> = Vector::with_capacity(bm.len());
+    for kp in bm.iter() {
+        from_points.push(
+            // these unwraps will never fail
+            source_keypoints
+                .get(kp.train_idx.try_into().unwrap())
+                .unwrap(),
+        );
+        to_points.push(
+            // these unwraps will never fail
+            scan_keypoints
+                .get(kp.query_idx.try_into().unwrap())
+                .unwrap(),
+        );
+    }
+    let mut from_p2f: Vector<Point2f> = Vector::with_capacity(bm.len());
+    KeyPoint::convert_def(&from_points, &mut from_p2f).unwrap();
+    let mut to_p2f: Vector<Point2f> = Vector::with_capacity(bm.len());
+    KeyPoint::convert_def(&to_points, &mut to_p2f).unwrap();
+    let mut result_mask = Mat::default();
+    find_homography(&from_p2f, &to_p2f, &mut result_mask, RANSAC, 10.0).unwrap()
+}
+
 impl ORBFlann for PaperPair {
     fn detect_transform(&self) -> Mat {
-        // find keypoints
-        let (source_keypoints, source_descriptors) = detect_and_compute_orb(
+        detect_transform_orb_flann(
             &self.source,
-            Some(self.sheet_data.gen_detect_mask(&self.source).unwrap()),
-        );
-        // for scanned images, the corresponding points should be near that area.
-        // We will search an area expanded a little (by ORB_ENLARGE_RECT_BY).
-        let (scan_keypoints, scan_descriptors) = detect_and_compute_orb(
             &self.scanned,
-            Some(
-                self.sheet_data
-                    .gen_enlarged_detect_mask(
-                        &self.scanned,
-                        ORB_ENLARGE_RECT_BY,
-                        ORB_ENLARGE_RECT_BY,
-                    )
-                    .unwrap(),
-            ),
-        );
-
-        // use flann matcher to find matches
-        let autoindexparams = flann::AutotunedIndexParams::new_def().expect("autotunedindexparams");
-        let flann_matcher = FlannBasedMatcher::new(
-            &Ptr::new(autoindexparams.into()),
-            &Ptr::new(flann::SearchParams::new_def().unwrap()),
+            &self.sheet_data,
+            ORB_ENLARGE_RECT_BY,
         )
-        .expect("Creating FlannBasedMatcher");
-        let mut src_desc = Mat::default();
-        let mut scan_desc = Mat::default();
-        // https://stackoverflow.com/a/29695032
-        source_descriptors
-            .convert_to_def(&mut src_desc, CV_32F)
-            .unwrap();
-        scan_descriptors
-            .convert_to_def(&mut scan_desc, CV_32F)
-            .unwrap();
-        // use the best matches in the visualisation
-        let bm: Vector<DMatch> = if PREFER_LOWE {
-            let mut matches = Vector::new();
-            flann_matcher
-                .knn_train_match_def(&scan_desc, &src_desc, &mut matches, 2)
-                .unwrap();
-            gather_good_matches_lowe(&matches, LOWE_DEFAULT_THRESHOLD)
-        } else {
-            let mut matches = Vector::new();
-            flann_matcher
-                .train_match_def(&scan_desc, &src_desc, &mut matches)
-                .unwrap();
-            gather_good_matches_take_n(&matches, ORB_FLANN_SHOW_N_BEST_MATCHES)
-        };
-        if DEBUG {
-            let mut result = Mat::default();
-            draw_matches_def(
-                &self.source,
-                &source_keypoints,
-                &self.scanned,
-                &scan_keypoints,
-                &bm,
-                &mut result,
-            )
-            .unwrap();
-            imshow("Matching result", &result).unwrap();
-            wait_key(0).unwrap();
-            imwrite(
-                "result.png",
-                &result,
-                &Vector::from_slice(&[ImwriteFlags::IMWRITE_PNG_COMPRESSION.into(), 9]),
-            )
-            .unwrap();
-        }
-
-        // find the transformation matrix
-        let mut from_points: Vector<KeyPoint> = Vector::with_capacity(bm.len());
-        let mut to_points: Vector<KeyPoint> = Vector::with_capacity(bm.len());
-        for kp in bm.iter() {
-            from_points.push(
-                // these unwraps will never fail
-                source_keypoints
-                    .get(kp.train_idx.try_into().unwrap())
-                    .unwrap(),
-            );
-            to_points.push(
-                // these unwraps will never fail
-                scan_keypoints
-                    .get(kp.query_idx.try_into().unwrap())
-                    .unwrap(),
-            );
-        }
-        let mut from_p2f: Vector<Point2f> = Vector::with_capacity(bm.len());
-        KeyPoint::convert_def(&from_points, &mut from_p2f).unwrap();
-        let mut to_p2f: Vector<Point2f> = Vector::with_capacity(bm.len());
-        KeyPoint::convert_def(&to_points, &mut to_p2f).unwrap();
-        let mut result_mask = Mat::default();
-        find_homography(&from_p2f, &to_p2f, &mut result_mask, RANSAC, 10.0).unwrap()
     }
 
     fn calc_diff(&self) {
